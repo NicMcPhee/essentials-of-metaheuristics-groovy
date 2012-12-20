328d3c925d5fd1b7db6509bc7e63031488cc3a41
Nic McPhee <mcphee@morris.umn.edu>
2012-12-20 00:27:51 -0600

    Added some tests for the basic hill climber.

---
+++ EssentialsOfMetaheuristics/test/singleStateMethods/HillClimberTest.groovy
@@ -0,0 +1,25 @@
+package singleStateMethods
+
+import problems.OnesMax
+import spock.lang.Specification
+
+class HillClimberTest extends Specification {
+	def "should work on small targets with plenty of evals"() {
+		final NUM_BITS = 10
+		def onesMax = new OnesMax(numBits : NUM_BITS, maxIterations : 1000)
+		def hillClimber = new HillClimber(problem : onesMax)
+		
+		expect:
+		hillClimber.maximize() == [1]*NUM_BITS
+	}
+	
+	def "won't work on large targets with few evals"() {
+		final NUM_BITS = 1000
+		def onesMax = new OnesMax(numBits : NUM_BITS, maxIterations : 100)
+		def hillClimber = new HillClimber(problem : onesMax)
+		
+		expect:
+		hillClimber.maximize() != [1]*NUM_BITS
+
+	}
+}
