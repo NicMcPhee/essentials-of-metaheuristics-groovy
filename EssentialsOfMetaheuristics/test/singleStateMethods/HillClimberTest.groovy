b1b3cf57280a80ec0e254c1a2a6019caf3749bd8
Nic McPhee <mcphee@morris.umn.edu>
2012-12-21 22:52:57 -0600

    Fixed a test that was broken when I changed the signature of searcher constructors.

--- EssentialsOfMetaheuristics/test/singleStateMethods/HillClimberTest.groovy
+++ EssentialsOfMetaheuristics/test/singleStateMethods/HillClimberTest.groovy
@@ -7,19 +7,19 @@ class HillClimberTest extends Specification {
 	def "should work on small targets with plenty of evals"() {
 		final NUM_BITS = 10
 		def onesMax = new OnesMax(numBits : NUM_BITS, maxIterations : 1000)
-		def hillClimber = new HillClimber(problem : onesMax)
+		def hillClimber = new HillClimber()
 		
 		expect:
-		hillClimber.maximize() == [1]*NUM_BITS
+		hillClimber.maximize(onesMax) == [1]*NUM_BITS
 	}
 	
 	def "won't work on large targets with few evals"() {
 		final NUM_BITS = 1000
 		def onesMax = new OnesMax(numBits : NUM_BITS, maxIterations : 100)
-		def hillClimber = new HillClimber(problem : onesMax)
+		def hillClimber = new HillClimber()
 		
 		expect:
-		hillClimber.maximize() != [1]*NUM_BITS
+		hillClimber.maximize(onesMax) != [1]*NUM_BITS
 
 	}
 }
