4a26ad4176bf81bbb2b73cf1f522276c53635d16
Nic McPhee <mcphee@morris.umn.edu>
2012-12-21 22:55:05 -0600

    Extracted a shared superclass from the two bitstring problems. Those problem classes were largely identical, so this allowed us to save on a ton of duplicated code.

--- EssentialsOfMetaheuristics/src/problems/OnesMax.groovy
+++ EssentialsOfMetaheuristics/src/problems/OnesMax.groovy
@@ -3,57 +3,9 @@ package problems
 import groovy.transform.ToString
 
 
-class OnesMax {
-	private rand = new java.util.Random()
-	Integer evalCount = 0
-	Integer maxIterations = 1000
-	Integer numBits = 1000
-
+class OnesMax extends BitStringProblem {
 	def quality = { a ->
 		++evalCount
 		a.count(1)
-	}
-	
-	def create = { n = numBits ->
-		// Makes an array of n zeros.
-		[0]*n 
-	}
-	
-	def copy = { a -> a.clone() }
-
-	/*
-	 * Having this take an option array of bits works, but probably isn't
-	 * super efficient, especially for large bit strings, as we need to allocate
-	 * memory for and construct the full set of bits, which is really not
-	 * necessary. An alternative would be to send in a closure that will return
-	 * a 0 or 1 every time it's called. The default closure could return random
-	 * bits, while for testing we could send in a closure that advances through
-	 * an fixed array of bits. For now, however, this works, so I'm going to leave
-	 * it alone and move on.
-	 */
-	def tweak = { a, randomBits = null ->
-		if (randomBits == null) {
-			randomBits = (0..<a.size()).collect {
-				// I'm using the common 1/N rule for mutation, i.e.,
-				// have the mutation probability be 1/N where N is the
-				// length of the bit string.
-				rand.nextInt(a.size()) == 0
-			}
-		}
-		(0..<a.size()).collect { i ->
-			if (randomBits[i]) {
-				1-a[i]
-			} else {
-				a[i]
-			}
-		}
-	}
-	
-	def terminate = { a, q = quality(a) ->
-		evalCount >= maxIterations || q == a.size()
-	}
-	
-	String toString() {
-		this.class.name.split("\\.")[-1] + "_" + numBits + "_" + maxIterations
 	}
 }
