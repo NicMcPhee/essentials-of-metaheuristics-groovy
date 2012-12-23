3d2d60498755d791a475b960425573d81b6bb007
Nic McPhee <mcphee@morris.umn.edu>
2012-12-22 18:27:20 -0600

    Complete - task : Implement Leading Ones Blocks
    Did some runs and SAHCWR definitely wins for block sizes 2 and 4. I
    looked at some larger numbers of samples and for both of these problems
    a sample size of 8 is the best of what I tried (1, 2, 4, 8, 16).

--- EssentialsOfMetaheuristics/src/experiments/ExperimentRunner.groovy
+++ EssentialsOfMetaheuristics/src/experiments/ExperimentRunner.groovy
@@ -1,6 +1,7 @@
 package experiments
 
 import problems.LeadingOnes
+import problems.LeadingOnesBlocks
 import problems.OnesMax
 import singleStateMethods.HillClimber
 import singleStateMethods.SteepestAscentHillClimber
@@ -22,17 +23,22 @@ class ExperimentRunner {
 
 	static main(args) {
 		def searchers = [
-			new HillClimber(),
-			new SteepestAscentHillClimber(numGradientSamples : 1),
-			new SteepestAscentHillClimber(numGradientSamples : 2),
-			new SteepestAscentHillClimber(numGradientSamples : 4),
-			new SteepestAscentHillClimberWithReplacement(numGradientSamples : 1),
-			new SteepestAscentHillClimberWithReplacement(numGradientSamples : 2),
-			new SteepestAscentHillClimberWithReplacement(numGradientSamples : 4),
+//			new HillClimber(),
+//			new SteepestAscentHillClimber(numGradientSamples : 1),
+//			new SteepestAscentHillClimber(numGradientSamples : 2),
+//			new SteepestAscentHillClimber(numGradientSamples : 4),
+//			new SteepestAscentHillClimberWithReplacement(numGradientSamples : 1),
+//			new SteepestAscentHillClimberWithReplacement(numGradientSamples : 2),
+//			new SteepestAscentHillClimberWithReplacement(numGradientSamples : 4),
+			new SteepestAscentHillClimberWithReplacement(numGradientSamples : 8),
+			new SteepestAscentHillClimberWithReplacement(numGradientSamples : 16)
 		]
 		def problems = [
-			new OnesMax(numBits : 100, maxIterations : 250),
-			new LeadingOnes(numBits : 100, maxIterations : 1000)
+			//new OnesMax(numBits : 100, maxIterations : 250),
+			//new LeadingOnes(numBits : 100, maxIterations : 1000)
+			// new LeadingOnesBlocks(numBits : 100, maxIterations : 10000, blockSize : 1), 
+			new LeadingOnesBlocks(numBits : 100, maxIterations : 10000, blockSize : 2), 
+			new LeadingOnesBlocks(numBits : 100, maxIterations : 10000, blockSize : 4) 
 		]
 		// It would be nice to collect the total time here and include it in the
 		// output.
---
+++ EssentialsOfMetaheuristics/src/problems/LeadingOnesBlocks.groovy
@@ -0,0 +1,24 @@
+package problems
+
+/*
+ * This is the same as LeadingOnes, but we divide by a block size b
+ * and then take the floor of the result. This means that the system
+ * has to find/solve an entire block before it receives any kind of
+ * reward via an increase in fitness.
+ */
+class LeadingOnesBlocks extends BitStringProblem {
+	Integer blockSize
+	
+	def quality = { a ->
+		++evalCount
+		Integer numOnes = 0
+		while (a[numOnes] == 1) {
+			++numOnes
+		}
+		return (int) (numOnes / blockSize)
+	}
+	
+	String toString() {
+		super.toString() + "_" + blockSize
+	}
+}
