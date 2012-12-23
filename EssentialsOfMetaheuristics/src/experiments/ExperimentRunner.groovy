55c61dec3f7e33f98d7cba01ba3ad7bb60286884
Nic McPhee <mcphee@morris.umn.edu>
2012-12-22 20:21:01 -0600

    Implemented a simple binary trap function.

--- EssentialsOfMetaheuristics/src/experiments/ExperimentRunner.groovy
+++ EssentialsOfMetaheuristics/src/experiments/ExperimentRunner.groovy
@@ -3,6 +3,7 @@ package experiments
 import problems.LeadingOnes
 import problems.LeadingOnesBlocks
 import problems.OnesMax
+import problems.Trap
 import singleStateMethods.HillClimber
 import singleStateMethods.SteepestAscentHillClimber
 import singleStateMethods.SteepestAscentHillClimberWithReplacement
@@ -23,13 +24,13 @@ class ExperimentRunner {
 
 	static main(args) {
 		def searchers = [
-//			new HillClimber(),
-//			new SteepestAscentHillClimber(numGradientSamples : 1),
-//			new SteepestAscentHillClimber(numGradientSamples : 2),
-//			new SteepestAscentHillClimber(numGradientSamples : 4),
-//			new SteepestAscentHillClimberWithReplacement(numGradientSamples : 1),
-//			new SteepestAscentHillClimberWithReplacement(numGradientSamples : 2),
-//			new SteepestAscentHillClimberWithReplacement(numGradientSamples : 4),
+			new HillClimber(),
+			new SteepestAscentHillClimber(numGradientSamples : 1),
+			new SteepestAscentHillClimber(numGradientSamples : 2),
+			new SteepestAscentHillClimber(numGradientSamples : 4),
+			new SteepestAscentHillClimberWithReplacement(numGradientSamples : 1),
+			new SteepestAscentHillClimberWithReplacement(numGradientSamples : 2),
+			new SteepestAscentHillClimberWithReplacement(numGradientSamples : 4),
 			new SteepestAscentHillClimberWithReplacement(numGradientSamples : 8),
 			new SteepestAscentHillClimberWithReplacement(numGradientSamples : 16)
 		]
@@ -37,8 +38,11 @@ class ExperimentRunner {
 			//new OnesMax(numBits : 100, maxIterations : 250),
 			//new LeadingOnes(numBits : 100, maxIterations : 1000)
 			// new LeadingOnesBlocks(numBits : 100, maxIterations : 10000, blockSize : 1), 
-			new LeadingOnesBlocks(numBits : 100, maxIterations : 10000, blockSize : 2), 
-			new LeadingOnesBlocks(numBits : 100, maxIterations : 10000, blockSize : 4) 
+//			new LeadingOnesBlocks(numBits : 100, maxIterations : 10000, blockSize : 2), 
+//			new LeadingOnesBlocks(numBits : 100, maxIterations : 10000, blockSize : 4) 
+			new Trap(numBits : 4, maxIterations : 1000), 
+			new Trap(numBits : 8, maxIterations : 1000), 
+			new Trap(numBits : 16, maxIterations : 1000), 
 		]
 		// It would be nice to collect the total time here and include it in the
 		// output.
---
+++ EssentialsOfMetaheuristics/src/problems/Trap.groovy
@@ -0,0 +1,28 @@
+package problems
+
+import groovy.transform.ToString
+
+
+class Trap extends BitStringProblem {
+	def create = { n = numBits ->
+		// Makes an array of random bits.
+		def result = []
+		for (i in 0..<n) {
+			result << rand.nextInt(2)
+		}
+		return result
+	}
+	
+	def quality = { a ->
+		++evalCount
+		def numZeros = a.count(0)
+		// If everything is a 1 (i.e., there are no zeros)
+		// then the fitness is n+1. Otherwise it's the number
+		// of zeros.
+		if (numZeros == 0) {
+			return a.size() + 1
+		} else {
+			return numZeros
+		}
+	}
+}
