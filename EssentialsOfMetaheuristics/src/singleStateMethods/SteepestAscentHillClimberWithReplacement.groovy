73be84ff4bcf1fa4cac5fd4334cccd29986db235
Nic McPhee <mcphee@morris.umn.edu>
2013-01-15 22:32:54 -0600

    Removed a spurious @ToString for this class, left over from experimenting with the automatic toString() method generation system.

--- EssentialsOfMetaheuristics/src/singleStateMethods/SteepestAscentHillClimberWithReplacement.groovy
+++ EssentialsOfMetaheuristics/src/singleStateMethods/SteepestAscentHillClimberWithReplacement.groovy
@@ -3,7 +3,7 @@ package singleStateMethods
 import problems.OnesMax
 import groovy.transform.ToString
 
-@ToString
+
 class SteepestAscentHillClimberWithReplacement {	
 	Integer numGradientSamples = 1
 	
