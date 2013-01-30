690086a3abf8f46b9d6176ba2c3816ea3f2cc5f4
giesk018 <giesk018@deathstar.morris.umn.edu>
2013-01-30 13:35:13 -0600

    I made a change. FIND IT.

--- EssentialsOfMetaheuristics/src/singleStateMethods/HillClimber.groovy
+++ EssentialsOfMetaheuristics/src/singleStateMethods/HillClimber.groovy
@@ -3,7 +3,7 @@ package singleStateMethods
 import problems.OnesMax
 import groovy.transform.ToString
 
-
+// STOLEN FROM NIC MCPHEE WHO STOLE IT FROM SEAN LUKE
 class HillClimber {		
 	// Happily this ended up being an almost direct copy from Sean's book.
 	def maximize(problem) {
