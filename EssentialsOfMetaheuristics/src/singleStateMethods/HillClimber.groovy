d28dd386bbfc53648abd63e4af2683d1d50172d3
perra044 <perra044@morris.umn.edu>
2013-01-30 13:33:54 -0600

    Intial commit

--- EssentialsOfMetaheuristics/src/singleStateMethods/HillClimber.groovy
+++ EssentialsOfMetaheuristics/src/singleStateMethods/HillClimber.groovy
@@ -4,6 +4,7 @@ import problems.OnesMax
 import groovy.transform.ToString
 
 
+/*  this is a clone from Nic by Matt */
 class HillClimber {		
 	// Happily this ended up being an almost direct copy from Sean's book.
 	def maximize(problem) {
