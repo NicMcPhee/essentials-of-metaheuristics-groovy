e6b3807d78b88a538ef409856d9c7c07f28df917
schil227 <schil227@mylar.morris.umn.edu>
2013-02-01 13:37:33 -0600

    Made the populationMethods package, Created the Algorithm19 class, started the algorithm

---
+++ EssentialsOfMetaheuristics/src/populationMethods/Algorithm19.groovy
@@ -0,0 +1,12 @@
+package populationMethods
+import problems.OnesMax
+
+class Algorithm19 {
+	def maximize(numParents, numChildren){
+		def randIndividualArr = []
+
+		for (i in 0..numParents) {
+			  
+			}
+	}
+}
