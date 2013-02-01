7febb60da1ff15f9485e5473275822b0b1a0ef32
schil227 <schil227@mylar.morris.umn.edu>
2013-02-01 14:06:53 -0600

    generated the list of individuals, calculated the best, and now are working on sorting/taking the m best individuals

--- EssentialsOfMetaheuristics/src/populationMethods/Algorithm19.groovy
+++ EssentialsOfMetaheuristics/src/populationMethods/Algorithm19.groovy
@@ -3,10 +3,39 @@ import problems.OnesMax
 
 class Algorithm19 {
 	def maximize(numParents, numChildren){
-		def randIndividualArr = []
-
+		def individualArr = []
+		
 		for (i in 0..numParents) {
-			  
+			individualArr.add(problem.create())	  
+		}
+		def best = individualArr[0]
+		def bestQuality = problem.quality(best)
+		while(!problem.terminate(best, problem.quality(best))) {
+			for (individual in individualArr) {
+				def quality = problem.quality(individual)
+				if (quality > bestQuality) {
+					best = individual
+				}
+				
 			}
+			
+			//Calculating the best of individualArr
+			for (ind in individualArr){
+				def indQuality = problem.quality(individual)
+				def index = 0
+				def sortedIndividualArr = []
+				if(sortedIndividualArr.empty){
+					sortedIndividualArr.add(ind)
+				} else {
+				   while(indQuality < problem.quality(sortedIndividualArr.get(index))){
+						index++
+				   	}
+				   //Left off here, add to the arr at the right index
+				   sortedIndividualArr.add(ind)
+				}
+				
+			}
+			
+		}
 	}
 }
