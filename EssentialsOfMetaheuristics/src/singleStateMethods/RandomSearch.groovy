2035d952935488cb259bce8ef618e4685e3bfa50
buck0337 <buck0337@corlar.morris.umn.edu>
2013-02-01 13:36:19 -0600

    Wrote RandomSearch

---
+++ EssentialsOfMetaheuristics/src/singleStateMethods/RandomSearch.groovy
@@ -0,0 +1,20 @@
+package singleStateMethods
+
+import problems.OnesMax
+import groovy.transform.ToString
+
+class RandomSearch {
+    def maximize(problem) {
+        def s = problem.random()
+        def sQuality = problem.quality(s)
+        while (!terminate(s, bestQuality)) {
+            def r = problem.random()
+            if (rQuality > sQuality) {
+                s = r
+                sQuality = rQuality
+            }
+        }
+        return s
+    }
+    
+}
