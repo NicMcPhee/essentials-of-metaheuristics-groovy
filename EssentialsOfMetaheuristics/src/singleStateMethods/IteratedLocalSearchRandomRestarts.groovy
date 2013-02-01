5816abf7b48c4726b8fdf6d34b0413cad3088890
latte048 <latte048@benzene.morris.umn.edu>
2013-02-01 13:32:43 -0600

    We created IteratedLocalSearchRandomRestarts.groovy (cloned from HillClimber.groovy) then updated the algorithm to perform iterated local searches.

---
+++ EssentialsOfMetaheuristics/src/singleStateMethods/IteratedLocalSearchRandomRestarts.groovy
@@ -0,0 +1,40 @@
+package singleStateMethods
+
+import problems.OnesMax
+import groovy.transform.ToString
+
+
+class IteratedLocalSearchRandomRestarts {
+	// Happily this ended up being an almost direct copy from Sean's book.
+	def maximize(problem) {
+		//def t = time distributions
+		def s = problem.create()
+		def sQuality = problem.quality(s)
+		def home = s
+		def homeQuality = sQuality
+		def best = s
+		def bestQuality = sQuality
+		while (!problem.terminate(s, sQuality)) {
+			//def time = t.sample
+			while(!time.terminate || !problem.terminate(s, sQuality)){ //should this be based on s or best?
+				def r = problem.tweak(problem.copy(s))
+				def rQuality = problem.quality(r)
+				if (rQuality > sQuality) {
+					s = r
+					sQuality = rQuality
+				}
+			}
+			if(sQuality > bestQuality){
+				best = s
+				bestQuality=sQuality
+			}
+			home = problem.newHomeBase(home, s) //Include homeQuality & sQuality?
+			s = problem.perturb(home)
+		}
+		return best
+	}
+
+	String toString() {
+		"ILS-RR"
+	}
+}
