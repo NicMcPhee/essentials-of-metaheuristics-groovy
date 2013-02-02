4a3f0b1f5e856fc7b81c1e348a8dcfc454972d68
schil227 <schil227@mylar.morris.umn.edu>
2013-02-02 13:37:05 -0600

    Finished implementation and did refactoring

--- EssentialsOfMetaheuristics/src/populationMethods/Algorithm19.groovy
+++ EssentialsOfMetaheuristics/src/populationMethods/Algorithm19.groovy
@@ -4,43 +4,47 @@ import problems.OnesMax
 class Algorithm19 {
 	def maximize(numParents, numChildren){
 		def individualArr = []
-		
+
 		for (i in 0..numParents) {
-			individualArr.add(problem.create())	  
+			individualArr.add(problem.create())
 		}
 		def best = individualArr[0]
-		def bestQuality = problem.quality(best)
+
 		while(!problem.terminate(best, problem.quality(best))) {
 			for (individual in individualArr) {
-				def quality = problem.quality(individual)
-				if (quality > bestQuality) {
+				if (problem.quality(individual) > problem.quality(best)) {
 					best = individual
 				}
-				
 			}
-			
+
 			//Calculating the best of individualArr
 			def sortedIndividualArr = []
 			for (ind in individualArr){
 				def indQuality = problem.quality(individual)
 				def index = 0
-				
+
 				if(sortedIndividualArr.empty){
 					sortedIndividualArr.add(0,ind)
 				} else {
-				   while(index <= sortedIndividualArr.size && indQuality < problem.quality(sortedIndividualArr.get(index))){
+					while(index <= sortedIndividualArr.size && indQuality < problem.quality(sortedIndividualArr.get(index))){
 						index++
-				   	}
-				   //Left off here, add to the arr at the right index
-				   sortedIndividualArr.add(index,ind)
+					}
+					//Left off here, add to the arr at the right index
+					sortedIndividualArr.add(index,ind)
 				}
-				
+
 			}
 			individualArr =[]
 			for (i in 0..numParents){
 				individualArr.add(sortedIndividualArr.get(i))
 			}
-			
+			for (ind in sortedIndividualArr) {
+				for (j in 0..(numChildren / numParents)) {
+					individualArr.add(problem.tweak(problem.copy(ind)))
+				}
+			}
+
 		}
+		return best
 	}
 }
