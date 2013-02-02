e74133ae33d2ea7c31e4915979f20b60fad1c8fa
schil227 <schil227@mylar.morris.umn.edu>
2013-02-02 14:47:26 -0600

    Ran tests, Feel like there may be some wierdness with our code (will consult nic) however feel as though it is working soundly.

--- EssentialsOfMetaheuristics/src/populationMethods/Algorithm19.groovy
+++ EssentialsOfMetaheuristics/src/populationMethods/Algorithm19.groovy
@@ -2,12 +2,16 @@ package populationMethods
 import problems.OnesMax
 
 class Algorithm19 {
-	def maximize(numParents, numChildren){
+	Integer numParents = 2
+	Integer numChildren = 5
+	def maximize(problem){
 		def individualArr = []
 
 		for (i in 0..numParents) {
 			individualArr.add(problem.create())
 		}
+		
+
 		def best = individualArr[0]
 
 		while(!problem.terminate(best, problem.quality(best))) {
@@ -20,13 +24,13 @@ class Algorithm19 {
 			//Calculating the best of individualArr
 			def sortedIndividualArr = []
 			for (ind in individualArr){
-				def indQuality = problem.quality(individual)
+				def indQuality = problem.quality(ind)
 				def index = 0
 
 				if(sortedIndividualArr.empty){
 					sortedIndividualArr.add(0,ind)
 				} else {
-					while(index <= sortedIndividualArr.size && indQuality < problem.quality(sortedIndividualArr.get(index))){
+					while(index < sortedIndividualArr.size && indQuality < problem.quality(sortedIndividualArr.get(index))){
 						index++
 					}
 					//Left off here, add to the arr at the right index
@@ -34,17 +38,25 @@ class Algorithm19 {
 				}
 
 			}
-			individualArr =[]
+			individualArr.clear()
 			for (i in 0..numParents){
 				individualArr.add(sortedIndividualArr.get(i))
 			}
-			for (ind in sortedIndividualArr) {
+			
+			for (i in 0..individualArr.size) {
 				for (j in 0..(numChildren / numParents)) {
-					individualArr.add(problem.tweak(problem.copy(ind)))
-				}
+					individualArr.add(problem.tweak(problem.copy(individualArr.get(i))))
+					}
 			}
+			sortedIndividualArr.clear()
+			
+			
 
 		}
 		return best
+	}
+
+	String toString() {
+		"Algorithm19_" + numParents + "_" + numChildren
 	}
 }
