80bb55f73aaaf074ded96906c8bcdda47ff97c6d
John McCall <mcca0798@avenger.morris.umn.edu>
2013-02-04 13:25:43 -0600

    Changed some stuff

--- EssentialsOfMetaheuristics/src/populationMethods/theGeneticAlgorithm.groovy
+++ EssentialsOfMetaheuristics/src/populationMethods/theGeneticAlgorithm.groovy
@@ -1,33 +1,37 @@
 package populationMethods
 
+import java.util.Random
+
 class theGeneticAlgorithm {
+	// Algorithm 20
+	
 	/** Our code makes several assumptions:
 	 * The problem has a newRandomIndividual method for creating new random individuals
 	 * The problem has an isIdeal method for checking if our solution is ideal
-	 * Individuals have a method assessFitness which will their fitness
+	 * The problem will have a method assessFitness
 	 * Individuals have a fitness method which will return their fitness
 	 * Individuals have a copy method
-	 * Either the Individuals or the problem gives us the methods: selectWithReplacement, crossover, and mutate
+	 * The Individual gives us the methods: crossover, and mutate
 	 */
 		
 	
 	// Our Algorithm takes a Genetic Algorithm Problem, a desired population size, and the number of runs to run
-	def geneticAlgorithm(GA, populationSize, numRuns) {
+	def geneticAlgorithm(GAP, populationSize, numRuns) {
 		def popsize = populationSize //can change if you like
 		
 		// Population
 		def startingPopulation = [] as Set
 		
 		for(i in 0..popsize){
-			def toAdd = GA.newRandomIndividual
+			def toAdd = GAP.newRandomIndividual
 			startingPopulation.add(toAdd) // Add a new random individual
 		}
 		
 		def best
 		def count = 0
-		while(GA.isIdeal(best) || (count == numRuns)) {
+		while(!GAP.isIdeal(best) && (count != numRuns)) {
 			for(def individual: startingPopulation) {
-				individual.assessFitness();
+				GAP.assessFitness(individual);
 				if(best == null || individual.getFitness() > best.getFitness()) {
 					best = individual
 				}
@@ -47,5 +51,11 @@ class theGeneticAlgorithm {
 			count++
 		}
 		return best
+	}
+	
+	def selectWithReplacement(population) {
+		def rand = new Random()
+		def randNumber = rand.nextInt(population.size())
+		return population[randNumber]	
 	}
 }
