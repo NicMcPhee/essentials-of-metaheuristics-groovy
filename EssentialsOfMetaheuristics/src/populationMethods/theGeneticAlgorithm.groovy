62ee3f6f2bc3bda95320d7b9f14f1d9f9b23ab22
John McCall <mcca0798@avenger.morris.umn.edu>
2013-02-04 13:52:21 -0600

    Wrote some stuff to reduce assumptions

--- EssentialsOfMetaheuristics/src/populationMethods/theGeneticAlgorithm.groovy
+++ EssentialsOfMetaheuristics/src/populationMethods/theGeneticAlgorithm.groovy
@@ -1,29 +1,28 @@
 package populationMethods
 
 import java.util.Random
+import EssentialsOfMetaheuristics.singleStateMethods.Crossover
 
 class theGeneticAlgorithm {
 	// Algorithm 20
 	
 	/** Our code makes several assumptions:
-	 * The problem has a newRandomIndividual method for creating new random individuals
+	 * The problem has a random method for creating new random individuals
 	 * The problem has an isIdeal method for checking if our solution is ideal
-	 * The problem will have a method assessFitness
 	 * Individuals have a fitness method which will return their fitness
-	 * Individuals have a copy method
-	 * The Individual gives us the methods: crossover, and mutate
 	 */
 		
 	
 	// Our Algorithm takes a Genetic Algorithm Problem, a desired population size, and the number of runs to run
 	def geneticAlgorithm(GAP, populationSize, numRuns) {
 		def popsize = populationSize //can change if you like
+		def crossover = new Crossover()
 		
 		// Population
 		def startingPopulation = [] as Set
 		
 		for(i in 0..popsize){
-			def toAdd = GAP.newRandomIndividual
+			def toAdd = GAP.random()
 			startingPopulation.add(toAdd) // Add a new random individual
 		}
 		
@@ -31,7 +30,7 @@ class theGeneticAlgorithm {
 		def count = 0
 		while(!GAP.isIdeal(best) && (count != numRuns)) {
 			for(def individual: startingPopulation) {
-				GAP.assessFitness(individual);
+				GAP.quality(individual);
 				if(best == null || individual.getFitness() > best.getFitness()) {
 					best = individual
 				}
@@ -43,9 +42,8 @@ class theGeneticAlgorithm {
 			for(i in 0..(popsize/2)) {
 				def parentA = selectWithReplacement(startingPopulation)
 				def parentB = selectWithReplacement(startingPopulation)
-				def childA = crossover(copy(parentA), copy(parentB))
-				def childB = crossover(copy(parentA), copy(parentB))
-				endingPopulation.add(mutate(childA), mutate(childB))
+				def children = crossover.twoPointCrossover(parentA, parentB)
+				endingPopulation.add(GAP.tweak(children[0]), GAP.tweak(children[1]))
 			}
 			startingPopulation = endingPopulation
 			count++
