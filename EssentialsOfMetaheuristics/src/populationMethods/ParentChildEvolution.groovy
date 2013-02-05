f8aae0f3cbf26c2a06e86ddba0b4525607326a4b
thom3706 <thom3706@corlar.morris.umn.edu>
2013-02-05 17:35:15 -0600

    Stuff Same as last time, only with the right file!

--- EssentialsOfMetaheuristics/src/populationMethods/ParentChildEvolution.groovy
+++ EssentialsOfMetaheuristics/src/populationMethods/ParentChildEvolution.groovy
@@ -1,48 +1,78 @@
 package populationMethods
- 
+
 import problems.OnesMax
 import groovy.transform.ToString
+import java.util.ArrayList
 
 class ParentChildEvolution {
 	Integer parents = 1
 	Integer children = 1
-	
+
 	def maximize(problem){
-		def childArr=[];
+		def child=[];
+		ArrayList childArr = new ArrayList()
 		for (int i=0; i<children;i++){
-			childArr.add(problem.create()) //a random thing?
+			child = (problem.create()) //a random thing?
+			childArr.add(child)
 		}
+		System.out.println(childArr)
 		def best = null
-		def bestQuality = null
+		def bestQuality = -1
 		def childFitnessMap
 		while(!problem.terminate(best, bestQuality)){ //while best is not ideal and we have time
 			childFitnessMap = new TreeMap()
-			for(stuff in childArr){		
-				childFitnessMap.put(problem.quality(stuff), stuff)
+			for(problemChild in childArr){
+				childFitnessMap.put(problem.quality(problemChild), problemChild)
 			}
-			def allkeys = childFitnessMap.keySet() //sorted in ascending order.
-			def alphaKeys=[];
-			for (def i = children-parents; i<children; i++){ //We need the best fitnesses: the last parent-number of keys.
-				alphaKeys+= allkeys[i]
-			}
-			
-			if (best == null ||  alphaKeys[parents-1]> bestQuality) {
-				best = childFitnessMap.get(alphaKeys[parents-1])
-				bestQuality = alphaKeys[parents-1]
+			System.out.println(childFitnessMap)
+			def allKeys = childFitnessMap.keySet() as List  //sorted in ascending order.
+			System.out.println(allKeys)
+
+			if (best == null ||  allKeys[allKeys.size()-1]> bestQuality) {
+				best = pullElement(childFitnessMap.getAt(allKeys[allKeys.size()-1]))
+				bestQuality = allKeys[allKeys.size()-1]
 			}
 			childArr=[]
-			def parent
-			for(key in alphaKeys){
-				parent=childFitnessMap.get(key)
+			def parentList = getParents(allKeys, childFitnessMap)
+			for(def j=0; j<parents; j++){
 				for(def i=0; i<children/parents; i++){
-					childArr+= problem.tweak(parent)
+					childArr+= problem.tweak(parentList[j])
 				}
 			}
 		}
 		return best
 	}
-	
+
+	def pullElement(arr, index = 0){
+		if(arr.class.isArray()){
+			arr[index]
+		}
+		else{arr}
+	}
+
+	def getParents(keys, map){
+		def parentList = []
+		def maxParents = 0
+		def whichKey = 1
+		while(maxParents < parents){
+			def nextParent = map.getAt(keys[keys.size()-whichKey])
+			if(nextParent.getClass()== ArrayList){
+				for(def i=0; i<nextParent.size() && i<parents; i++){
+					parentList += nextParent[i]
+					maxParents++
+				}
+			}
+			else{
+				parentList += nextParent
+				maxParents++
+			}
+			whichKey++
+		}
+		System.out.println(parentList)
+		parentList
+	}
+
 	String toString(){
 		"(" + parents+","+children+")Evo"
 	}
-} 
+}
