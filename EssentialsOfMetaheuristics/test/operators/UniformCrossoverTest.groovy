2eefcd94542de3534a5e89dd81975baeadf34f72
giesk018 <giesk018@deathstar.morris.umn.edu>
2013-02-06 13:03:44 -0600

    Added a mirror image test in UniformCrossoverTest

--- EssentialsOfMetaheuristics/test/operators/UniformCrossoverTest.groovy
+++ EssentialsOfMetaheuristics/test/operators/UniformCrossoverTest.groovy
@@ -30,4 +30,20 @@ class UniformCrossoverTest extends Specification {
 			output[1].count(0) == output[0].count(1)
 			output[1].count(1) == output[0].count(0)
 	}
+	
+	def "Output strings are mirror images of each other"() {
+		given:
+			def bitstring1 = [0, 0, 0, 0]
+			def bitstring2 = [1, 1, 1, 1]
+		when:
+			def output = uniformCrossover.crossover(bitstring1, bitstring2)
+		then:
+			for (i in 0 ..< bitstring1.size()) {
+				if (output[0][i] == 0) {
+					output[1][i] == 1
+				} else {
+					output[1][i] == 0
+				}
+			}
+	}
 }
