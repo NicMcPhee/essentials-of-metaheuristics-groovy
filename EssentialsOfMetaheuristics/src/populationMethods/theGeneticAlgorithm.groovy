779bfe4d3e9b11404f508213558b3d191f3dddc9
John McCall <mcca0798@avenger.morris.umn.edu>
2013-02-06 13:58:53 -0600

    Changed selectWithReplacement to be a selector which is passed in as an argument

--- EssentialsOfMetaheuristics/src/populationMethods/theGeneticAlgorithm.groovy
+++ EssentialsOfMetaheuristics/src/populationMethods/theGeneticAlgorithm.groovy
@@ -1,36 +1,36 @@
 package populationMethods
 
 import java.util.Random
-import EssentialsOfMetaheuristics.singleStateMethods.Crossover
+import singleStateMethods.Crossovers
 
 class TheGeneticAlgorithm {
 	// Algorithm 20
 	
 	/** Our code makes several assumptions:
 	 * The problem has a random method for creating new random individuals
-	 * The problem has an isIdeal method for checking if our solution is ideal
-	 * Individuals have a fitness method which will return their fitness
 	 */
 		
 	
 	// Our Algorithm takes a Genetic Algorithm Problem, a desired population size, and the number of runs to run
-	def geneticAlgorithm(GAP, populationSize) {
-		def popsize = populationSize //can change if you like
+	def geneticAlgorithm(problem, populationSize, selectionMethod) {
+		def popsize = populationSize //can change if need be
 		def crossover = new Crossover()
+		def selector = selectionMethod
 		
 		// Population
 		def startingPopulation = [] as Set
 		
 		for(i in 0..popsize){
-			def toAdd = GAP.random()
+			def toAdd = problem.random()
 			startingPopulation.add(toAdd) // Add a new random individual
 		}
 		
 		// Start best as just some random individual
-		def best = GAP.random()
-		while(!terminate(best, GAP.quality(best))) {
+		def best = problem.random()
+		while(!terminate(best, problem.quality(best))) {
+			problem.population = startingPopulation
 			for(def individual: startingPopulation) {
-				GAP.quality(individual);
+				problem.quality(individual);
 				if(individual.getFitness() > best.getFitness()) {
 					best = individual
 				}
@@ -40,19 +40,14 @@ class TheGeneticAlgorithm {
 			def endingPopulation = [] as Set
 			
 			for(i in 0..(popsize/2)) {
-				def parentA = selectWithReplacement(startingPopulation)
-				def parentB = selectWithReplacement(startingPopulation)
+				def parentA = selector.maximize(problem, startingPopulation)
+				def parentB = selector.maximize(problem, startingPopulation)
 				def children = crossover.twoPointCrossover(parentA, parentB)
-				endingPopulation.add(GAP.tweak(children[0]), GAP.tweak(children[1]))
+				endingPopulation.add(problem.tweak(children[0]), problem.tweak(children[1]))
 			}
 			startingPopulation = endingPopulation
 		}
 		return best
 	}
-	
-	def selectWithReplacement(population) {
-		def rand = new Random()
-		def randNumber = rand.nextInt(population.size())
-		return population[randNumber]	
-	}
+
 }
