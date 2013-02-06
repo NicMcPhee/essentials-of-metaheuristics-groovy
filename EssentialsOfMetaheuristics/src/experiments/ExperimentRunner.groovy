3dba96eafe0433a7d8607b0f84394354640eb345
VincentBorchardt <borch135@morris.umn.edu>
2013-02-06 13:40:16 -0600

    reverted back ExperimentRunner

--- EssentialsOfMetaheuristics/src/experiments/ExperimentRunner.groovy
+++ EssentialsOfMetaheuristics/src/experiments/ExperimentRunner.groovy
@@ -6,7 +6,6 @@ import problems.LeadingOnesBlocks
 import problems.OnesMax
 import problems.Trap
 import singleStateMethods.HillClimber
-import singleStateMethods.IteratedLocalSearchRandomRestarts
 import singleStateMethods.SteepestAscentHillClimber
 import singleStateMethods.SteepestAscentHillClimberWithReplacement
 
@@ -18,7 +17,7 @@ class ExperimentRunner {
 				for (i in 0..<numRuns) {
 					p.evalCount = 0
 					def result = s.maximize(p)
-					println "${s.toString()}\t${p.toString()}\t${p.quality(result)}"
+					println "${s.toString()}\t${p.toString()}\t${p.quality(result)}\t${result}"
 				}
 			}
 		}
@@ -27,19 +26,17 @@ class ExperimentRunner {
 	static main(args) {
 		def searchers = [
 			new HillClimber(),
-			//new SteepestAscentHillClimber(numGradientSamples : 1),
+			new SteepestAscentHillClimber(numGradientSamples : 1),
 			new SteepestAscentHillClimber(numGradientSamples : 2),
-			//new SteepestAscentHillClimber(numGradientSamples : 4),
-			//new SteepestAscentHillClimberWithReplacement(numGradientSamples : 1),
-			//new SteepestAscentHillClimberWithReplacement(numGradientSamples : 2),
+			new SteepestAscentHillClimber(numGradientSamples : 4),
+			new SteepestAscentHillClimberWithReplacement(numGradientSamples : 1),
+			new SteepestAscentHillClimberWithReplacement(numGradientSamples : 2),
 			new SteepestAscentHillClimberWithReplacement(numGradientSamples : 4),
-			//new SteepestAscentHillClimberWithReplacement(numGradientSamples : 8),
-			//new SteepestAscentHillClimberWithReplacement(numGradientSamples : 16)
-			new IteratedLocalSearchRandomRestarts()
+			new SteepestAscentHillClimberWithReplacement(numGradientSamples : 8),
+			new SteepestAscentHillClimberWithReplacement(numGradientSamples : 16)
 		]
 		def problems = [
-			new OnesMax(numBits : 10, maxIterations : 100),
-			new OnesMax(numBits : 100, maxIterations : 100)
+//			new OnesMax(numBits : 100, maxIterations : 250),
 //			new LeadingOnes(numBits : 100, maxIterations : 1000),
 //			new LeadingOnesBlocks(numBits : 100, maxIterations : 10000, blockSize : 1), 
 //			new LeadingOnesBlocks(numBits : 100, maxIterations : 10000, blockSize : 2), 
@@ -47,10 +44,10 @@ class ExperimentRunner {
 //			new Trap(numBits : 4, maxIterations : 1000), 
 //			new Trap(numBits : 8, maxIterations : 1000), 
 //			new Trap(numBits : 16, maxIterations : 1000), 
-			//new HIFF(numBits : 4, maxIterations : 1000),
-			//new HIFF(numBits : 8, maxIterations : 1000),
-			//new HIFF(numBits : 16, maxIterations : 1000),
-			//new HIFF(numBits : 32, maxIterations : 1000)
+			new HIFF(numBits : 4, maxIterations : 1000),
+			new HIFF(numBits : 8, maxIterations : 1000),
+			new HIFF(numBits : 16, maxIterations : 1000),
+			new HIFF(numBits : 32, maxIterations : 1000)
 		]
 		// It would be nice to collect the total time here and include it in the
 		// output.
