d62235bb300f71758a227af6005f1cab54a29908
John McCall <mcca0798@avenger.morris.umn.edu>
2013-02-08 12:27:29 -0600

    Changed some things that Nic brought up on GitHub.

--- EssentialsOfMetaheuristics/src/populationMethods/GeneticAlgorithm.groovy
+++ EssentialsOfMetaheuristics/src/populationMethods/GeneticAlgorithm.groovy
@@ -3,41 +3,36 @@ package populationMethods
 import java.util.Random
 import singleStateMethods.Crossovers
 
-class TheGeneticAlgorithm {
+class GeneticAlgorithm {
 	// Algorithm 20
-	
-	/** Our code makes several assumptions:
-	 * The problem has a random method for creating new random individuals
-	 */
-		
 	
 	// Our Algorithm takes a Genetic Algorithm Problem, a desired population size, and the number of runs to run
 	def geneticAlgorithm(problem, populationSize, selectionMethod) {
-		def popsize = populationSize //can change if need be
+		def popsize = populationSize
 		def crossover = new Crossover()
 		def selector = selectionMethod
 		
 		// Population
-		def startingPopulation = [] as Set
+		def startingPopulation = []
 		
 		for(i in 0..popsize){
 			def toAdd = problem.random()
 			startingPopulation.add(toAdd) // Add a new random individual
 		}
 		
-		// Start best as just some random individual
-		def best = problem.random()
-		while(!terminate(best, problem.quality(best))) {
-			problem.population = startingPopulation
+		def best = problem.create()
+		def qualityOfBest = problem.quality(best)
+		while(!problem.terminate(best, qualityOfBest)) {
 			for(def individual: startingPopulation) {
-				problem.quality(individual);
-				if(individual.getFitness() > best.getFitness()) {
+				def newQuality = problem.quality(individual)
+				if(newQuality > qualityOfBest) {
 					best = individual
+					qualityOfBest = newQuality
 				}
 				
 			}
 			
-			def endingPopulation = [] as Set
+			def endingPopulation = []
 			
 			for(i in 0..(popsize/2)) {
 				def parentA = selector.maximize(problem, startingPopulation)
