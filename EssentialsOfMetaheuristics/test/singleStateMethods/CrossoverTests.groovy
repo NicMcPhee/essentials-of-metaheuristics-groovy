127fa50a4953d5f6fb25d9a0a424fd2ac44c8aee
Nic McPhee <nic.mcphee@gmail.com>
2013-02-09 20:23:15 -0600

    Added tests that check that the crossover operators don't modify the parents. These currently fail, indicating a significant problem with these operators.

--- EssentialsOfMetaheuristics/test/singleStateMethods/CrossoverTests.groovy
+++ EssentialsOfMetaheuristics/test/singleStateMethods/CrossoverTests.groovy
@@ -13,6 +13,48 @@ class CrossoverTests extends Specification {
     }
     def father = [1, 0, 1, 0, 1, 0, 1, 0]
     def mother = [0, 1, 0, 1, 0, 1, 0, 1]
+	
+	def "One point crossover shouldn't change either parent"() {
+		given:
+		def fatherClone = father.clone()
+		def motherClone = mother.clone()
+		def crossovers = new Crossovers()
+		
+		when:
+		def children = crossovers.onePointCrossover(father, mother)
+		
+		then:
+		father == fatherClone
+		mother == motherClone
+	}
+    
+	def "Two point crossover shouldn't change either parent"() {
+		given:
+		def fatherClone = father.clone()
+		def motherClone = mother.clone()
+		def crossovers = new Crossovers()
+		
+		when:
+		def children = crossovers.twoPointCrossover(father, mother)
+		
+		then:
+		father == fatherClone
+		mother == motherClone
+	}
+    
+	def "Uniform point crossover shouldn't change either parent"() {
+		given:
+		def fatherClone = father.clone()
+		def motherClone = mother.clone()
+		def crossovers = new Crossovers()
+		
+		when:
+		def children = crossovers.uniformCrossover(father, mother)
+		
+		then:
+		father == fatherClone
+		mother == motherClone
+	}
     
     def"total number of ones and zeroes should not change"(){
     
