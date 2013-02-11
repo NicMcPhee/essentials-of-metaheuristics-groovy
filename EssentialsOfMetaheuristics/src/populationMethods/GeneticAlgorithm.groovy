b4d5da01aae85007a10f749ad3d524714765c885
John McCall <mcca0798@avenger.morris.umn.edu>
2013-02-11 14:09:25 -0600

    Fixed stuff. Crossovers is using closures now.

--- EssentialsOfMetaheuristics/src/populationMethods/GeneticAlgorithm.groovy
+++ EssentialsOfMetaheuristics/src/populationMethods/GeneticAlgorithm.groovy
@@ -1,21 +1,25 @@
 package populationMethods
 
 import java.util.Random
-import singleStateMethods.Crossovers
+import operators.Crossovers
+import singleStateMethods.TournamentSelection
 
 class GeneticAlgorithm {
 	// Algorithm 20
+	// crossover is assumed to be a closure
+	def crossover = (new Crossovers()).onePointCrossover
+	def selector = new TournamentSelection
 	
 	// Our Algorithm takes a Genetic Algorithm Problem, a desired population size, and the number of runs to run
-	def geneticAlgorithm(problem, populationSize, selectionMethod) {
+	def geneticAlgorithm(problem, populationSize, selectionMethod=selector, crossoverMethod=crossover) {
 		def popsize = populationSize
-		def crossover = new Crossover()
-		def selector = selectionMethod
-		
+		selector=selectionMethod
+		crossover = crossoverMethod
+	
 		// Population
 		def startingPopulation = [] as Set
 		
-		for(i in 0..popsize){
+		popsize.times {
 			def toAdd = problem.random()
 			startingPopulation.add(toAdd) // Add a new random individual
 		}
@@ -35,9 +39,9 @@ class GeneticAlgorithm {
 			def endingPopulation = [] as Set
 			
 			for(i in 0..(popsize/2)) {
-				def parentA = selector.maximize(problem, startingPopulation)
-				def parentB = selector.maximize(problem, startingPopulation)
-				def children = crossover.twoPointCrossover(parentA, parentB)
+				def parentA = selector.select(problem, startingPopulation)
+				def parentB = selector.select(problem, startingPopulation)
+				def children = crossover(parentA, parentB)
 				endingPopulation.add(problem.tweak(children[0]), problem.tweak(children[1]))
 			}
 			startingPopulation = endingPopulation
