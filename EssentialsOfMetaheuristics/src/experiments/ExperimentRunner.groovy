803521f36444751e0d006cd704b8545537b2539a
Josh Johnson <cobar1337@gmail.com>
2013-02-10 21:16:29 -0600

    Testing

--- EssentialsOfMetaheuristics/src/experiments/ExperimentRunner.groovy
+++ EssentialsOfMetaheuristics/src/experiments/ExperimentRunner.groovy
@@ -17,7 +17,7 @@ class ExperimentRunner {
 				for (i in 0..<numRuns) {
 					p.evalCount = 0
 					def result = s.maximize(p)
-					println "${s.toString()}\t${p.toString()}\t${p.quality(result)}"
+					println "${s.toString()}\t${p.toString()}\t${p.quality(result)}\t${result}"
 				}
 			}
 		}
@@ -32,12 +32,25 @@ class ExperimentRunner {
 			new SteepestAscentHillClimberWithReplacement(numGradientSamples : 1),
 			new SteepestAscentHillClimberWithReplacement(numGradientSamples : 2),
 			new SteepestAscentHillClimberWithReplacement(numGradientSamples : 4),
+			new SteepestAscentHillClimberWithReplacement(numGradientSamples : 8),
+			new SteepestAscentHillClimberWithReplacement(numGradientSamples : 16)
 		]
 		def problems = [
-			new OnesMax(numBits : 10, maxIterations : 100),
-			new OnesMax(numBits : 100, maxIterations : 100),
-			new OnesMax(numBits : 1000, maxIterations : 100),
+//			new OnesMax(numBits : 100, maxIterations : 250),
+//			new LeadingOnes(numBits : 100, maxIterations : 1000),
+//			new LeadingOnesBlocks(numBits : 100, maxIterations : 10000, blockSize : 1), 
+//			new LeadingOnesBlocks(numBits : 100, maxIterations : 10000, blockSize : 2), 
+//			new LeadingOnesBlocks(numBits : 100, maxIterations : 10000, blockSize : 4),
+//			new Trap(numBits : 4, maxIterations : 1000), 
+//			new Trap(numBits : 8, maxIterations : 1000), 
+//			new Trap(numBits : 16, maxIterations : 1000), 
+			new HIFF(numBits : 4, maxIterations : 1000),
+			new HIFF(numBits : 8, maxIterations : 1000),
+			new HIFF(numBits : 16, maxIterations : 1000),
+			new HIFF(numBits : 32, maxIterations : 1000)
 		]
+		// It would be nice to collect the total time here and include it in the
+		// output.
 		runExperiment(searchers, problems)
 	}
 
