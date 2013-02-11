6dddc983a4b7773d1e7e8d2649d83dd010a05936
LordCobar <cobar1337@gmail.com>
2013-02-10 22:03:31 -0600

    Copied directly from Nic's master file.

--- EssentialsOfMetaheuristics/src/singleStateMethods/HillClimber.groovy
+++ EssentialsOfMetaheuristics/src/singleStateMethods/HillClimber.groovy
@@ -1,20 +1,11 @@
 package singleStateMethods
-//This comment makes me happy
+
 import problems.OnesMax
 import groovy.transform.ToString
 
-/* I love swedish meatballs */
 
-//Cloned from Nic
-
-// CLONED FROM NIC!
-//This is a clone of Nic's files
-// STOLEN FROM NIC MCPHEE WHO STOLE IT FROM SEAN LUKE
-//Hola from Phou
-/*cloned from nic */
-// This is a clone!!!!1
 class HillClimber {		
-        //  Happily this ended up being an almost direct copy from Sean's book.
+	// Happily this ended up being an almost direct copy from Sean's book.
 	def maximize(problem) {
 		def s = problem.create()
 		def sQuality = problem.quality(s)
@@ -28,7 +19,7 @@ class HillClimber {
 		}
 		return s
 	}
-	// clowns 
+	
 	String toString() {
 		"HC"
 	}
