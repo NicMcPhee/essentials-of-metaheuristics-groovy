d313037a5a1c3224c5310a06fa0d36fe2eea3b39
malon153 <malon153@reliant.morris.umn.edu>
2013-02-13 14:33:24 -0600

    Fixed default numParents and NumChildren to be something potentially useful (5,20). Fixed references to GenomeFitnessPairs. Made Best and BestQuality into a GenomeFitnessPair. Actually removed old FitnessValuePair class from repository.

--- EssentialsOfMetaheuristics/src/populationMethods/MuCommaLambdaEvolution.groovy
+++ EssentialsOfMetaheuristics/src/populationMethods/MuCommaLambdaEvolution.groovy
@@ -4,39 +4,37 @@ import problems.OnesMax
 import groovy.transform.ToString
 
 class MuCommaLambdaEvolution {
-	Integer numParents = 1
-	Integer numChildren = 1
+	Integer numParents = 5
+	Integer numChildren = 20
 
 	def maximize(problem){
 		assert numParents > 0 && numChildren >0, "numParents and numChildren must be positive"
 		assert numChildren%numParents == 0, "numChildren must be a multiple of numParents"
-		def childArr = [] //will be an array of [fitness,value] pairs
+		def childArr = []
 		numChildren.times{
-			def candidateSolution = problem.create()
-			childArr.add(new FitnessValuePair(value:candidateSolution,fitness:problem.quality(candidateSolution)))
+			def candidateSolution = problem.random()
+			childArr.add(new GenomeFitnessPair(genome:candidateSolution,fitness:problem.quality(candidateSolution)))
 		}
-		def best = childArr[0].value
-		def bestQuality = childArr[0].fitness
+		def best = new GenomeFitnessPair(genome: childArr[0].genome, fitness:childArr[0].fitness)
 
-		while(!problem.terminate(best, bestQuality)){ //while best is not ideal and we have time
+		while(!problem.terminate(best.genome, best.fitness)){ //while best is not ideal and we have time
 			childArr.sort()
 			if (childArr[0].fitness > bestQuality) {
-				best = childArr[0].value
-				bestQuality = childArr[0].fitness
+				best = childArr[0]
 			}
 			def parentList = []
 			numParents.times{i->
-				parentList.add(childArr[i].value)
+				parentList.add(childArr[i].genome)
 			}
 			childArr= []
 			for(parent in parentList){
 				(numChildren/numParents).times{
 					def nextSolution = problem.tweak(parent)
-					childArr.add(new FitnessValuePair(value:nextSolution, fitness:problem.quality(nextSolution)))
+					childArr.add(new GenomeFitnessPair(genome:nextSolution, fitness:problem.quality(nextSolution)))
 				}
 			}
 		}
-		return best
+		return best.genome
 	}
 
 	String toString(){
