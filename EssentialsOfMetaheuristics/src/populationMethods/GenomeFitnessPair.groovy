9d184debdbb593ea6d89c6fa60717e710f77855d
malon153 <malon153@reliant.morris.umn.edu>
2013-02-13 14:26:10 -0600

    Renamed FitnessValuePair to GenomeFitnessPair, renamed field from value to genome.

---
+++ EssentialsOfMetaheuristics/src/populationMethods/GenomeFitnessPair.groovy
@@ -0,0 +1,19 @@
+package populationMethods
+
+@Immutable class GenomeFitnessPair implements Comparable{
+	protected def genome
+	protected def fitness
+
+	//This is set so that default sort methods will return the pairs sorted in descending order by fitness
+	public int compareTo(pair2) {
+		if(this.fitness > pair2.fitness){
+			-1
+		}
+		else if (this.fitness < pair2.fitness){
+			1
+		}
+		else{0}
+	}
+
+
+}
