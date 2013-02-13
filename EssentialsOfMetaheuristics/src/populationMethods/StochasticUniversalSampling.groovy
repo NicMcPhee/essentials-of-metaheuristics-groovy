607d1029431bbff21aa0a7f056aa5eda501122c1
long0385 <long0385@acrylic.morris.umn.edu>
2013-02-13 17:46:07 -0600

    Made more changes and think we are finished.

--- EssentialsOfMetaheuristics/src/populationMethods/StochasticUniversalSampling.groovy
+++ EssentialsOfMetaheuristics/src/populationMethods/StochasticUniversalSampling.groovy
@@ -4,32 +4,24 @@ import java.util.Random;
 class StochasticUniversalSampling {
 
 
-    //global index
-    def index = 0
-
-    //global value
-    def value
-
     //random number generator
-    def randomInt = new Random()
+    def numGenerator = new Random()
 
     //method for shuffling vectors (shuffles both p and f to preserve order)
     def shuffle(vector1, vector2){
-        def i
 
-        def j
 
         //Length of vector1 and vector2 should be the same since each individual has a corresponding fitness.
         def vectLength = vector1.length
 
-        for (i = vectLength; i >= 2; i--){
-            j = randomInt.next(vectLength-i)+i
-            def temp1 = vector1.i
-            vector1.i = vector1.j
-            vector1.j = temp1
-            def temp2 = vector2.i
-            vector2.i = vector2.j
-            vector2.j = temp2
+        for (def i = vectLength; i >= 2; i--){
+            def j = numGenerator.next(vectLength-i)+i
+            def temp1 = vector1[i]
+            vector1[i] = vector1[j]
+            vector1[j] = temp1
+            def temp2 = vector2[i]
+            vector2[i] = vector2[j]
+            vector2[j] = temp2
         }
 
     }
@@ -37,6 +29,7 @@ class StochasticUniversalSampling {
     def sus(populationSize, populationVector, fitnessVector){
         for(int b = 0; b < populationSize; b++){
             shuffle(populationVector,fitnessVector)
+            def index = 0
             def allZeros = true
             for(int i = 0; i <= fitnessVector.length; i++){
                 if (fitnessVector[i] != 0){
@@ -50,7 +43,7 @@ class StochasticUniversalSampling {
             for (int a = 1; a < fitnessVector.length; a++){
                 fitnessVector[a] = fitnessVector[a] + fitnessVector[a-1]
             }
-            value = randomInt.next(fitnessVector[fitnessVector.length-1]/populationSize)
+            def value = numGenerator.next(fitnessVector[fitnessVector.length-1]/populationSize)
         } 
             while (fitnessVector[index] < value){
             index++
