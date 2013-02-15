d7146c5e9a06000484d03ccf7c0b8444bfa394fc
Braden Luthi <luthi003@acrylic.morris.umn.edu>
2013-02-14 18:41:22 -0600

    made loops more groovy, added methods to build and normalize a fitness Array.

--- EssentialsOfMetaheuristics/src/FitnessProportionateSelection/FitnessProportionateSelection.groovy
+++ EssentialsOfMetaheuristics/src/FitnessProportionateSelection/FitnessProportionateSelection.groovy
@@ -4,12 +4,49 @@ class FitnessProportionateSelection {
 
     public double select(problem, population) {
         def sum = 0
-        def decision = Math.random()
-        for(int i = 0; i< population.size();i++) {
-            sum += problem.quality(population[i])
+        def fitIndex = 0
+         def decision = Math.random()
+         build a population fitness array
+        def fitnessArray = buildPopFitness(problem,population)
+        // normalize fitness array
+        fitnessArray = normalize(fitnessArray)
+        for(person in population) {
+            sum += fitnessArray[fitIndex]
+            fitIndex++
             if(decision <= sum){
-               return population[i]
+                return person
             }
         }
+    }
+    //
+    // Builds population fitness array
+    //
+    private buildPopFitness(problem,population){
+        def fitnessArray= []
+        for(person in population){
+            fitnessArray.add(problem.quality(person))
+        }
+        fitnessArray
+    }
+    //
+    // Normalizes fitness array
+    //
+    private normalize(fitnessArr){
+        def normFits = []
+        def sum = normalizedSum(fitnessArr)
+        for(fitness in fitnessArr){
+            normFits.add(fitness*sum)
+        }
+        normFits
+    }
+    //
+    // derives normalization ratio
+    //
+    private normalizedSum(fitnessArr){
+        def sum =0
+        for(fitness in fitnessArr){
+            sum+=fitness
+        }
+        return new BigDecimal(1/sum)
     }
 }
