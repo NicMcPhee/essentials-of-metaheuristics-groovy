745c35f178ab127b42a0cf448c48b3a37c83a6ba
Alex Gunness <gunn0056>
2013-02-22 16:00:19 -0600

    fixed griewank test

--- EssentialsOfMetaheuristics/test/problems/GriewankTest.groovy
+++ EssentialsOfMetaheuristics/test/problems/GriewankTest.groovy
@@ -12,9 +12,10 @@ class GriewankTest extends Specification {
         griewank.create(numValues, [Math.PI*Math.PI/4]) == [Math.PI*Math.PI/4]
     }
 
-//    def "Quality Function Works Correctly"() {
-//        expect:
+    def "Quality Function Works Correctly"() {
+        expect:
         //Since floats are prone to roundoff error, here instead of using == 0 we used a range
-//        griewank.quality([Math.PI*Math.PI/4]) < -(Math.PI*Math.PI/16000 + 1) + 0.00000001 && griewank.quality([Math.PI*Math.PI]) > -(Math.PI*Math.PI/16000 + 1) - 0.00000001
-//    }
+        griewank.quality([Math.PI*Math.PI/4]) < -0.22031012493691826 + 0.00000001 && griewank.quality([Math.PI*Math.PI]) > -0.22031012493691826 - 0.00000001
+    }
 }
+
