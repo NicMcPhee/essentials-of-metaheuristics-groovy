b22b17088e9dae5afeb88159bb9d3b03c923b879
Nic McPhee <mcphee@morris.umn.edu>
2013-04-01 00:43:40 -0500

    This is a first stab at adding the creation of battle files and the running of battles from within the EC code. It doesn't currently work because Robocode can't seem to find the class files. Using the Robocode GUI to package the code into a jar file seems to fix the problem, which makes me think that the issue is actually the fact that there's an inner class. I need to test that to see, however.

---
+++ EssentialsOfMetaheuristics/src/applications/robocode/BattleRunner.groovy
@@ -0,0 +1,50 @@
+package applications.robocode
+
+import groovy.text.SimpleTemplateEngine
+
+class BattleRunner {
+    def userHome = System.getProperty("user.home")
+    def robotDirectory = "evolved_robots"
+    def robotDirectoryAbsolute = new File(robotDirectory).absolutePath
+    def template
+
+    def BattleRunner(String templateFileName) {
+        def engine = new SimpleTemplateEngine()
+        template = engine.createTemplate(new File(templateFileName))
+    }
+
+    def buildBattleFile(id) {
+        File battleFile = new File("${robotDirectory}/evolve.battle")
+        battleFile.delete()
+        battleFile.createNewFile()
+        def result = template.make(["id" : id])
+        battleFile << result.toString()
+    }
+
+    def runBattle(id) {
+        linkClassFiles(id)
+        File battleFile = new File("${robotDirectory}/evolve.battle")
+        def command = "${userHome}/robocode/robocode.sh -battle ${battleFile.absolutePath} -nodisplay"
+        def proc = command.execute(null, new File(robotDirectory))
+        proc.waitFor()
+        assert proc.exitValue() == 0
+        assert proc.err.text.equals("")
+        def lines = proc.in.text.split("\n")
+        lines.each { line ->
+            def m = line =~ /evolved\.Individual_${id}\s+(\d+)/
+            if (m) {
+                return m[0][1]
+            }
+        }
+    }
+    
+    def linkClassFiles(id) {
+        def robotDir = new File("${userHome}/robocode/robots/evolved/")
+        robotDir.mkdir()
+        def command = "ln -s ${robotDirectoryAbsolute}/evolved/Individual_${id}.class ${robotDirectoryAbsolute}/evolved/Individual_${id}\$MicroEnemy.class ."
+        def proc = command.execute(null, robotDir)
+        proc.waitFor()
+        assert proc.exitValue() == 0
+        assert proc.err.text.equals("")
+    }
+}
---
+++ EssentialsOfMetaheuristics/test/applications/robocode/TestRoboCodeBattle.groovy
@@ -0,0 +1,66 @@
+package applications.robocode
+
+import spock.lang.Specification
+
+class TestRoboCodeBattle extends Specification {
+	/*
+	 * id : an id used in the generation of the name of the class.
+	 * enemy_energy : the coefficient for the enemy's energy
+	 * my_energy : the coefficient for our energy
+	 * angle_diff : the coefficient for the different in angles between us and the point and then and the point
+	 * distance : the coefficient for the distance between the point and the enemy
+	 */
+	def id
+	def enemy_energy
+	def my_energy
+	def angle_diff
+	def distance
+	def robotBuilder
+	def battleRunner
+
+	def setup() {
+		Random random = new Random()
+		id = random.nextInt(1000000)
+		enemy_energy = random.nextFloat() * 100
+		my_energy = random.nextFloat() * 100
+		angle_diff = random.nextFloat() * 100
+		distance = random.nextFloat() * 100
+        def values = ["id" : id, "enemy_energy" : enemy_energy, "my_energy" : my_energy, "angle_diff" : angle_diff, "distance" : distance]
+
+		robotBuilder = new RobotBuilder("templates/HawkOnFireOS.template")
+		robotBuilder.buildClassFile(values)
+		
+		battleRunner = new BattleRunner("templates/battle.template")
+	}
+	
+	def "Check that the battle file is correctly constructed"() {
+		when:
+		battleRunner.buildBattleFile(id)
+		
+		then:
+		confirmBattleFile()
+	}
+	
+	def "Check that we can run a battle and extract the scores"() {
+		given:
+		battleRunner.buildBattleFile(id)
+		
+        when:
+		def score = battleRunner.runBattle(id)
+
+        then:
+		score >= 0
+	}
+	
+	def confirmBattleFile() {
+		File file = new File("evolved_robots/evolve.battle")
+        def contents = file.readLines()
+        def interestingLines = contents.findAll { line ->
+            (line.indexOf("robocode.battle.selectedRobots") >= 0)
+        }
+        assert interestingLines.size() == 1
+		assert interestingLines[0].indexOf("sample.Walls") >= 0
+        assert interestingLines[0].indexOf("evolved.Individual_${id}") >= 0
+        return true
+	}
+}
